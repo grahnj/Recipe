69959939e725df734aeb6cdb561c343c21522102
grahnj <grahnjus@outlook.com>
2015-05-05 19:22:51 -0500

    Update CreateSteps.js
    added functionality to create steps should now populate the text area with recipe steps as well as give the correct step number.

--- ui/CreateSteps.js
+++ ui/CreateSteps.js
@@ -1,5 +1,29 @@
-function CreateSteps(){
+function CreateSteps(readWrite, Recipe, stepNum, details) {
+	var recipe = Recipe;
+	var readWrite = readWrite;
 	var ui = Ti.UI;
+	var num = stepNum;
+	var steps = [];
+	var db = require("db");
+	var recipe = require("Ingredient");
+	var detail = " ";
+	var moreSteps = true;
+	
+	if (num+1 == steps.length) {
+		moreSteps = false;
+	}
+	
+	if(readWrite == false) {
+		if(details == null) {
+			var rows = db.getSteps();
+			while(rows.isValidRow()) {
+				steps.push(rows.fieldByName("DETAILS"));
+				rows.next();
+			}
+		}
+		detail = steps[num];
+	}
+	
 	//Create the window
 	var window = ui.createWindow({
 		backgroundColor: "#cccccc",
@@ -42,7 +66,7 @@ function CreateSteps(){
 			//Create a Label -- Step #
 			var headerLabel = ui.createLabel({
 				color: "#000000",
-				text: "Step #", //TODO This could contain dynamic information
+				text: "Step #" + num + 1, //TODO maybe change to show id of current step
 				height: ui.SIZE,
 				textAlign: "center",
 				top: "5%",
@@ -60,13 +84,15 @@ function CreateSteps(){
 			  borderColor: '#000000',
 			  borderRadius: 5,
 			  color: '#000000',
+			  editable: readWrite;
 			  font: {fontSize:20, fontWeight:'bold'},
 			  //keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD,
 			  returnKeyType: Ti.UI.RETURNKEY_GO,
 			  textAlign: 'left',
 			  hintText: 'Enter step information here...',
 			  top: 5,
-			  width: '100%', height : '80%',			  
+			  width: '100%', height : '80%',	
+			  value: detail	  
 			});
 			
 			stepsView.add(stepsArea);
@@ -87,7 +113,22 @@ function CreateSteps(){
 			   title: 'Next Step',
 			   top: 5,
 			   width: ui.SIZE,
+			   enabled: moreSteps
 			});
+			
+				nextButton.addEventListener('click', function(e) {
+					var CreateSteps = require('ui/CreateSteps');
+					if(readWrite) {
+						db.setStep(recipe.id, num+1, detail)
+						new CreateSteps(true, recipe, stepNum+1, null).open();
+					
+					}
+					else {
+						if(moreSteps) {
+						new CreateSteps(false, recipe, num+1, steps).open();
+						}
+					}
+				});
 			
 			buttonView.add(nextButton);
 			//Create a button -- finishButton
@@ -102,6 +143,7 @@ function CreateSteps(){
 		mainView.add(buttonView);
 		
 	window.add(mainView);
+		
 	
 	//window.open();
 	return window;
